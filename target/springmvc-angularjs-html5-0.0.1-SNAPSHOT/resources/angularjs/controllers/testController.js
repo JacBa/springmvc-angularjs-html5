6fd5d396997c722636b2e02707491e3e76851325
abouba <aj.ba@linuxmail.org>
2016-03-22 16:20:10 -0400

    clean repo

--- target/springmvc-angularjs-html5-0.0.1-SNAPSHOT/resources/angularjs/controllers/testController.js
+++
@@ -1,8 +0,0 @@
-//this is the definition for a new angularjs single page application
-
-var app = angular.module('myApp', []).controller('NewController', function($scope) {
-    $scope.newMethod = function() {    	
-    	//external controller is called
-        $scope.controllerCalled = true;
-    }
-});
